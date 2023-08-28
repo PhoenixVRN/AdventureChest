9385b6b01b91189d0b635d0d680e838b476e42d9
Evgenyi <VRN.Ocenka@gmail.com>
2023-08-28 15:59:07 +0300

    step 1

---
+++ Assets/Scripts/CardInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardInfo : MonoBehaviour
+{
+    public RawImage image;
+    public string name;
+    public TypeCard typeCard;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
